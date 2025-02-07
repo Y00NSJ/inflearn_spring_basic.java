983cb0dbf658fb624b4d3313adbaff09a616d7e6
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-08 08:29:03 +0900

    2-2. 테스트 코드 작성
    - 현재 도메인의 문제
      - 저장소 변경 시 OCP 위배
      - DIP 위배: 코드 내에서 구현체 지정(의존)
      - 인터페이스 뿐만 아니라 구현체에도 의존중

---
+++ src/main/java/ysj/core/MemberApp.java
@@ -0,0 +1,18 @@
+package ysj.core;
+
+import ysj.core.member.Grade;
+import ysj.core.member.Member;
+import ysj.core.member.MemberService;
+import ysj.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member : " + member.getName());
+        System.out.println("find member : " + findMember.getName());
+    }
+}
