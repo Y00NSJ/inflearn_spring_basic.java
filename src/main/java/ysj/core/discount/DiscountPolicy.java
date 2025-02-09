18c28f276c326266c22b2c705f6dbbe3c2e3ba7e
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-10 04:35:56 +0900

    2-3. 주문 및 할인 도메인 개발
    - DiscountPolicy 인터페이스 구현: 추후 할인 정책 변경 시 구현체 갈아끼우기
    - FixDiscountPolicy : 정액 할인 정책
    ↪️ 할인 정책을 담당하는 클래스를 주문 로직과 분리함으로써 SRP 준수
    - Order 도메인 구현: {주문자 id, 상품명, 정가, 할인가}, toString 메소드 구현
    - OrderService 인터페이스 구현
    - OrderServiceImpl 구현: 받은 memberId로 회원 찾은 후 discountPolicy 객체의 discount 메소드 호출, Order 객체 생성해 리턴

---
+++ src/main/java/ysj/core/discount/DiscountPolicy.java
@@ -0,0 +1,11 @@
+package ysj.core.discount;
+
+import ysj.core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
---
+++ src/main/java/ysj/core/order/OrderServiceImpl.java
@@ -0,0 +1,22 @@
+package ysj.core.order;
+
+import ysj.core.discount.DiscountPolicy;
+import ysj.core.discount.FixDiscountPolicy;
+import ysj.core.member.Member;
+import ysj.core.member.MemberRepository;
+import ysj.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        // 할인 관련 로직은 전혀 모름, discountPolicy에게 책임 전가
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 등급만 넘겨도 되나, 확장성 고려한 설계
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
