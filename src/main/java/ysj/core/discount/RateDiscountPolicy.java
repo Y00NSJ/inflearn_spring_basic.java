ef0ac6de0034d3d5385c3167bdf7d366d10f8725
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-10 12:47:36 +0900

    3-1. 할인 정책 확장: 정률 할인 정책
    - 비즈니스 로직 변화에 따라 새로운 할인 정책 추가 구현

---
+++ src/main/java/ysj/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package ysj.core.discount;
+
+import ysj.core.member.Grade;
+import ysj.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
