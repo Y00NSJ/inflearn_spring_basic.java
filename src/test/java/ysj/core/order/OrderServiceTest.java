12d1aec8c0bf8d783e846a165fd71e0196e7af75
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-10 13:55:52 +0900

    3-3. 리팩토링 : 실행부에서 AppConfig 사용
    - AppConfig 객체 생성 후, 해당 객체의 내부 메소드를 통해 클라이언트 객체 생성
    - 클라이언트에서 필요한 의존 관계는 AppConfig 내 클라이언트 객체 생성 메소드에서 주입

--- src/test/java/ysj/core/order/OrderServiceTest.java
+++ src/test/java/ysj/core/order/OrderServiceTest.java
@@ -1,7 +1,9 @@
 package ysj.core.order;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ysj.core.AppConfig;
 import ysj.core.member.Grade;
 import ysj.core.member.Member;
 import ysj.core.member.MemberService;
@@ -9,8 +11,15 @@ import ysj.core.member.MemberServiceImpl;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
