e29c43aa6bba638226e4d434d0dfc569384ab30a
Yoon Seo Jin <ysj0509@sookmyung.ac.kr>
2025-02-10 13:54:27 +0900

    3-3. 관심사 분리: 클라이언트 코드에서, 의존 관계 설정 부분 제거
    - 추상 인터페이스에만 의존, 생성자를 통해 외부에서 구현체 주입될 것
    - 앱 실행 클래스에서 AppConfig를 통해 클라이언트 객체 생성될 것
    - 내부 로직에만 집중

--- src/main/java/ysj/core/member/MemberServiceImpl.java
+++ src/main/java/ysj/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package ysj.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository; // 추상에만 의존
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
